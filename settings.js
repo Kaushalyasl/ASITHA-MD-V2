e6259f86d211a60c286573ab68fcb75d27fa7782
Kaushalyasl <149307067+Kaushalyasl@users.noreply.github.com>
2024-12-20 21:30:28 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=rqAUhTwa#eO3-c9EYwvqqVyWj4G7tWXM6vLDiCAWEZC2rIvNDcm4' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=zjQTBJLa#PNskmmcflN4nNzmjYuA438w8gOsSb60ggUd5X103xPM' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
